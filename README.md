af21b43248af683c29634f6bc01d269c88eea7fc
mwhidayat <hidayatmochamadwahyu@gmail.com>
2023-04-24 21:23:53 +0700

    Changed Readme; added Procfile, Req, and App files.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# indonesian-news-scraper
-This is a Python web scraping application that is built using the Streamlit framework, and is designed to retrieve news articles from a variety of Indonesian news publishers.
+## Indonesian News Article Scraper
+
+The Indonesian News Article Scraper is a Python web scraping application that is built using the Streamlit framework, and is designed to retrieve news articles from a variety of Indonesian news publishers. The app uses Google Search as the primary search mechanism, and searches for news articles that are related to the user's query string. The user can specify the number of pages they want to scrape, and also provide optional start and end dates to filter results by date range.
+
+## Features
+
+* Retrieval of news article data from a number of popular Indonesian publishers
+* Filtering of search results by date range (start and end dates)
+* Export of data to CSV format for further analysis
+
+## System Requirements
+
+* Python 3.7 or higher
+* Internet connection for scraping news article data
+
+## Installation
+
+1. Clone the repository to your local machine.
+2. Install the required dependencies by running the command `pip install -r requirements.txt`
+
+## Usage
+
+1. Navigate to the directory where the app is stored.
+2. Run the app by executing the command `streamlit run app.py`.
+3. In the app interface, enter a search query keyword or phrase in the text input box provided.
+4. Select the publisher's domain where you want to search for news from the available options in the radio button.
+5. (Optional) Enter the start and end date in the provided text input boxes to scrape articles within a specific date range. The format should be in yyyy-mm-dd.
+6. Enter the number of pages to scrape in the text input box provided. Each page usually contains 10 articles.
+7. Click the "Submit" button to start the scraping process.
+8. Once the scraping process is complete, the resulting dataframe containing information on the publication date, title, text, URL, and publication name will be displayed.
+9. If you wish to download the data as a CSV, click on the "Export to CSV" button located below the resulting dataframe.
+
+## License
+
+This project is licensed under the terms of the MIT license.
---
+++ app.py
@@ -0,0 +1,180 @@
+import streamlit as st
+import requests
+import time
+from bs4 import BeautifulSoup
+from newspaper import Article
+import pandas as pd
+import secrets
+from tqdm import tqdm
+import base64
+
+
+def get_urls_from_google(query, publisher, num_scrolls, start_date=None, end_date=None):
+    urls = []
+    query_string = f"{query} site:{publisher}"
+    if start_date:
+        query_string += f" after:{start_date}"
+    if end_date:
+        query_string += f" before:{end_date}"
+    url = f"https://www.google.co.id/search?q={query_string}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    while num_scrolls:
+        results = soup.find_all("div", class_="g")
+        for result in results:
+            link = result.find("a")["href"]
+            urls.append(link)
+        try:
+            next_button = soup.find("a", {"id": "pnnext"})["href"]
+            url = f"https://www.google.co.id{next_button}"
+            page = requests.get(url, headers=headers)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            time.sleep(2)  # Wait for new results to load
+        except:
+            break
+        num_scrolls -= 1
+    return urls
+
+def filter_links(urls, publisher):
+    filtered_links = []
+    for url in urls:
+        if f"{publisher}/tag/" not in url and f"{publisher}/topic/" not in url:
+            filtered_links.append(url)
+    return filtered_links
+
+def scrape_articles(filtered_urls):
+    rows = []
+    for filtered_url in tqdm(filtered_urls):
+        try:
+            a = Article(filtered_url,language='id')
+            a.download()
+            a.parse()
+            
+            date = a.publish_date
+            title = a.title
+            text = a.text
+            
+            unique_identifier = secrets.token_hex(20)
+            
+            row = {'Datetime':date,
+                   'Title':title,
+                   'Text':text,
+                   'URL':filtered_url,
+                   'TextID': unique_identifier,
+                   'Publication': publisher}
+    
+            rows.append(row)
+        except Exception as e:
+            print(e)
+            row = {'Datetime':'N/A',
+                   'Title':'N/A',
+                   'Text':'N/A',
+                   'URL':filtered_url,
+                   'TextID': 'N/A',
+                   'Publication': publisher}               
+            
+            rows.append(row)      
+    df = pd.DataFrame(rows)
+    return df
+
+def download_csv(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="articles.csv">Export to CSV</a>'
+    return href
+
+def app():
+    st.title("Indonesian news articles scraper")
+    
+    # Sidebar
+    query = st.text_input("Enter your search query:")
+    if not query:
+        st.stop()
+        
+    publisher_options = ['bisnis.com', 'cnbcindonesia.com', 'detik.com', 'kompas.com', 'liputan6.com', 'merdeka.com', 'okezone.com', 'suara.com', 'tribunnews.com']
+    publisher = st.radio("Select a publisher's domain:", publisher_options)
+    if not publisher:
+        st.stop()
+    
+    start_date = st.text_input("Enter the start date (yyyy-mm-dd) (optional):")
+    end_date = st.text_input("Enter the end date (yyyy-mm-dd) (optional):")
+    
+    num_scrolls = st.text_input("Enter the number of pages to scrape (each page usually contains 10 articles):")
+    if not num_scrolls:
+        st.stop()
+    num_scrolls = int(num_scrolls)
+    
+    # Get the URLs from Google search
+    with st.spinner("Getting search results..."):
+        urls = get_urls_from_google(query, publisher, num_scrolls, start_date, end_date)
+    st.success(f"{len(urls)} URLs for '{query}' from '{publisher}' are found.")
+    
+    # Filter URLs
+    with st.spinner("Filtering URLs..."):
+        filtered_urls = filter_links(urls, publisher)
+    st.success(f"Tagging URLs are found and removed. Filtering down to {len(filtered_urls)} articles and dropping results with NaN or null values.")
+    
+    # Scrape articles
+    rows = []
+    with st.spinner("Scraping articles..."):
+        for filtered_url in tqdm(filtered_urls):
+            try:
+                a = Article(filtered_url, language='id')
+                a.download()
+                a.parse()
+
+                date = a.publish_date
+                title = a.title
+                text = a.text
+
+                unique_identifier = secrets.token_hex(8)
+
+                row = {'Datetime': date,
+                       'Title': title,
+                       'Text': text,
+                       'URL': filtered_url,
+                       'TextID': unique_identifier,
+                       'Publication': publisher}
+
+                rows.append(row)
+            except Exception as e:
+                print(e)
+                row = {'Datetime': 'N/A',
+                       'Title': 'N/A',
+                       'Text': 'N/A',
+                       'URL': filtered_url,
+                       'TextID': 'N/A',
+                       'Publication': publisher}
+
+                rows.append(row)
+    df = pd.DataFrame(rows)
+    
+    # drop the rows with NaN, null or None
+    df.dropna(inplace=True)
+
+    # Replace line breaks in the Text column
+    df['Text'] = df['Text'].str.replace('\n', '  ')
+
+    
+    # Add progress bar
+    with st.spinner("Processing data..."):
+        progress_bar = st.progress(0)
+        for i in range(100):
+            time.sleep(0.01)
+            progress_bar.progress(i+1)
+    
+    # Show dataframe
+    st.write(df)
+    st.markdown(download_csv(df), unsafe_allow_html=True)
+
+    with st.container():
+        st.markdown("---")
+        st.markdown("Developed by [MW Hidayat](https://twitter.com/casecrit)")
+
+# Run the Streamlit app
+if __name__ == '__main__':
+    st.set_page_config(page_title='Article Scraper', page_icon=':newspaper:')
+    app()
