2b85a931ba4831f1ed665ca326fee6295a7fc0d6
mwhidayat <hidayatmochamadwahyu@gmail.com>
2023-05-06 09:25:17 +0700

    Add delay for scraper; add text analysis features.

--- app.py
+++ app.py
@@ -7,7 +7,20 @@ import pandas as pd
 from cryptography.fernet import Fernet
 from tqdm import tqdm
 import base64
+import streamlit as st
+import pandas as pd
+import string
+from text_analysis import plot_n_most_frequent_words
+import plotly.express as px
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from text_analysis import stopwords_removal
+from text_analysis import extract_collocations
+from text_analysis import display_concordance
+import time
 
+import random
+import time
 
 def get_urls_from_google(query, publisher, num_scrolls, start_date=None, end_date=None):
     urls = []
@@ -32,7 +45,8 @@ def get_urls_from_google(query, publisher, num_scrolls, start_date=None, end_dat
             url = f"https://www.google.co.id{next_button}"
             page = requests.get(url, headers=headers)
             soup = BeautifulSoup(page.content, 'html.parser')
-            time.sleep(2)  # Wait for new results to load
+            delay = random.uniform(3, 9)  # Wait for a random time between 3 and 9 seconds
+            time.sleep(delay)
         except:
             break
         num_scrolls -= 1
@@ -81,103 +95,319 @@ def scrape_articles(filtered_urls):
     df = pd.DataFrame(rows)
     return df
 
-def download_csv(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="articles.csv">Export to CSV</a>'
-    return href
+def load_data(file):
+    data = pd.read_csv(file, usecols=['Datetime', 'Title', 'Text', 'URL', 'TextID', "Publication"])
+    return data
 
 def app():
-    st.title("Indonesian news articles scraper")
-    
-    # Sidebar
-    query = st.text_input("Enter your search query:")
-    if not query:
-        st.stop()
+    st.title("SemanText")
+
+    # Features
+    option = st.sidebar.selectbox("Select a feature", ["Scraper", "Wordlist and Wordcloud", "Collocation", "Key Words in Context"])
+    if option == "Scraper":
+        st.markdown("---") 
+        st.subheader("Scraper")       
+        query = st.text_input("Enter your search query:")
+        if not query:
+            st.stop()
+            
+        publisher_options = ['bisnis.com', 'cnbcindonesia.com', 'cnnindonesia.com', 'detik.com', 'inews.id', 'jpnn.com', 'kompas.com', 'liputan6.com',  'merdeka.com', 'okezone.com', 'suara.com', 'tribunnews.com']
+        publisher = st.radio("Select a publisher's domain:", publisher_options)
+        if not publisher:
+            st.stop()
         
-    publisher_options = ['bisnis.com', 'cnbcindonesia.com', 'detik.com', 'kompas.com', 'liputan6.com', 'merdeka.com', 'okezone.com', 'suara.com', 'tribunnews.com']
-    publisher = st.radio("Select a publisher's domain:", publisher_options)
-    if not publisher:
-        st.stop()
-    
-    start_date = st.text_input("Enter the start date (yyyy-mm-dd) (optional):")
-    end_date = st.text_input("Enter the end date (yyyy-mm-dd) (optional):")
-    
-    num_scrolls = st.text_input("Enter the number of pages to scrape (each page usually contains 10 articles):")
-    if not num_scrolls:
-        st.stop()
-    num_scrolls = int(num_scrolls)
-    
-    # Get the URLs from Google search
-    with st.spinner("Getting search results..."):
-        urls = get_urls_from_google(query, publisher, num_scrolls, start_date, end_date)
-    st.success(f"{len(urls)} URLs for '{query}' from '{publisher}' are found.")
-    
-    # Filter URLs
-    with st.spinner("Filtering URLs..."):
-        filtered_urls = filter_links(urls, publisher)
-    st.success(f"If any tagging URLs and rows with null values are found, they will be removed.")
-    
-    # Scrape articles
-    rows = []
-    with st.spinner("Scraping articles..."):
-        for filtered_url in tqdm(filtered_urls):
+        start_date = st.text_input("Enter the start date (yyyy-mm-dd) (optional):")
+        end_date = st.text_input("Enter the end date (yyyy-mm-dd) (optional):")
+        
+        num_scrolls = st.text_input("Enter the number of pages to scrape (each page usually contains 10 articles):")
+        if not num_scrolls:
+            st.stop()
+        num_scrolls = int(num_scrolls)
+        
+        # Get the URLs from Google search
+        with st.spinner("Getting search results..."):
+            urls = get_urls_from_google(query, publisher, num_scrolls, start_date, end_date)
+        st.success(f"{len(urls)} URLs for '{query}' from '{publisher}' are found.")
+        
+        # Filter URLs
+        with st.spinner("Filtering URLs..."):
+            filtered_urls = filter_links(urls, publisher)
+        st.success(f"If any tagging URLs and rows with null values are found, they will be removed.")
+        
+        # Scrape articles
+        rows = []
+        start_time = time.time()
+        with st.spinner("Scraping articles..."):
+            for filtered_url in tqdm(filtered_urls):
+                try:
+                    a = Article(filtered_url, language='id')
+                    a.download()
+                    a.parse()
+
+                    date = a.publish_date
+                    title = a.title
+                    text = a.text
+
+                    key = Fernet.generate_key()
+                    unique_identifier = base64.urlsafe_b64encode(key).rstrip(b'=').hex()[:16]
+
+                    row = {'Datetime': date,
+                        'Title': title,
+                        'Text': text,
+                        'URL': filtered_url,
+                        'TextID': unique_identifier,
+                        'Publication': publisher}
+
+                    rows.append(row)
+                except Exception as e:
+                    print(e)
+                    row = {'Datetime': 'N/A',
+                        'Title': 'N/A',
+                        'Text': 'N/A',
+                        'URL': filtered_url,
+                        'TextID': 'N/A',
+                        'Publication': publisher}
+
+                    rows.append(row)
+        end_time = time.time()
+        duration = round(end_time - start_time, 2)
+        print(f"Scraping {len(rows)} articles took {duration} seconds.")
+        
+        df = pd.DataFrame(rows)
+        
+        # drop the rows with N/A, NaN, null or None
+        df = df[~df.isin(['N/A']).any(axis=1)]
+        df.dropna(inplace=True)
+        
+        # Add progress bar
+        with st.spinner("Processing data..."):
+            progress_bar = st.progress(0)
+            for i in range(100):
+                time.sleep(0.01)
+                progress_bar.progress(i+1)
+
+        # Replace line breaks with spaces
+        if 'Text' in df.columns:
+            df['Text'] = df['Text'].str.replace('\n', '  ')
+
+        # Show dataframe
+        st.write(df)
+
+        # Show scraping duration
+        st.success(f"Scraping {len(rows)} articles took {duration} seconds.")
+
+        # Download the scraped articles
+        def download_csv(df):
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()
+            href = f'<a href="data:file/csv;base64,{b64}" download="corpus_{query}_{publisher}.csv">Export to CSV</a>'
+            return href
+        st.markdown(download_csv(df), unsafe_allow_html=True)
+
+    elif option == "Wordlist and Wordcloud":
+        st.markdown("---")
+        st.subheader("Wordlist and Wordcloud")
+        # Add a file uploader for the CSV files
+        uploaded_files = st.file_uploader("Upload CSV files", accept_multiple_files=True, type=["csv"])
+
+        # Merge the uploaded CSV files into a single DataFrame
+        if uploaded_files:
+            corpus = pd.DataFrame() # Initialize an empty DataFrame to hold the merged corpus
+            total_words = 0 # Initialize a variable to hold the total number of words
+            for file in uploaded_files:
+                file.seek(0) # Reset the file pointer to the beginning of the file
+                data = pd.read_csv(file)
+                corpus = pd.concat([corpus, data], ignore_index=True)
+                total_words += data["Text"].str.split().str.len().sum() # Compute the number of words for the current file
+            
+           
+            # Display the total number of rows and words
+            st.write(f"Total articles: {len(corpus)}")
+            st.write(f"Total words: {total_words}")
+            
+            # Display an overview of the number of rows per value of the Publication column
+            publication_counts = corpus["Publication"].value_counts().to_frame().reset_index()
+            publication_counts.columns = ["Publication", "Count"] # Rename the columnsz
+            st.write("Articles by Publication:")
+            st.table(publication_counts)
+            st.markdown("---")
+            st.markdown("Merged dataframe")
+            st.write(corpus)
+
+        # Create a table for the most frequent words
+        if 'corpus' in locals() and st.button("Show most frequent words"):
+            n_words = st.text_input("Maximum words", "20")
             try:
-                a = Article(filtered_url, language='id')
-                a.download()
-                a.parse()
+                n_most_common = int(n_words)
+            except ValueError:
+                st.write("Please enter a valid integer for the number of words.")
+            
+            # Remove stop words and punctuation from the corpus
+            corpus['Text'] = corpus['Text'].apply(lambda x: stopwords_removal(x.lower().split()))
 
-                date = a.publish_date
-                title = a.title
-                text = a.text
+            top_n_words = plot_n_most_frequent_words(corpus, "Text", n=n_most_common)
 
-                key = Fernet.generate_key()
-                unique_identifier = base64.urlsafe_b64encode(key).rstrip(b'=').hex()[:16]
+            # Create a table for the most frequent words
+            words_freq = pd.DataFrame(top_n_words, columns=["Word", "Frequency"])
+            # Remove apostrophes and commas from the words in the table
+            words_freq["Word"] = words_freq["Word"].str.replace("'", "").str.replace(",", "")
+            # Remove the row that contains an empty string in the Word column
+            words_freq = words_freq[words_freq["Word"] != ""]
+            st.table(words_freq)
+            st.set_option('deprecation.showPyplotGlobalUse', False)
 
-                row = {'Datetime': date,
-                       'Title': title,
-                       'Text': text,
-                       'URL': filtered_url,
-                       'TextID': unique_identifier,
-                       'Publication': publisher}
+            # Create a horizontal bar chart using Plotly
+            fig = px.bar(
+                words_freq,
+                x='Frequency',
+                y='Word',
+                orientation='h',
+                labels={'y': 'Word', 'x': 'Frequency'}
+            )
+            fig.update_layout(
+                title=f"Most frequent words",
+                xaxis_title="Frequency",
+                yaxis_title="Word",
+                autosize=False,
+                width=800,
+                height=500
+            )
+            st.plotly_chart(fig)
+            st.set_option('deprecation.showPyplotGlobalUse', False)
 
-                rows.append(row)
-            except Exception as e:
-                print(e)
-                row = {'Datetime': 'N/A',
-                       'Title': 'N/A',
-                       'Text': 'N/A',
-                       'URL': filtered_url,
-                       'TextID': 'N/A',
-                       'Publication': publisher}
+        # Generate the word cloud
+        if 'corpus' in locals() and st.button("Generate word cloud"):
+            # Filter out any empty or NaN values from the corpus
+            corpus = corpus.dropna(subset=['Text'])
+            corpus = corpus[corpus['Text'] != '']
 
-                rows.append(row)
-    df = pd.DataFrame(rows)
-    
-    # drop the rows with NaN, null or None
-    df.dropna(inplace=True)
-    
-    # Add progress bar
-    with st.spinner("Processing data..."):
-        progress_bar = st.progress(0)
-        for i in range(100):
-            time.sleep(0.01)
-            progress_bar.progress(i+1)
+            # Remove stop words and punctuation from the corpus
+            corpus['Text'] = corpus['Text'].apply(lambda x: stopwords_removal([word.translate(str.maketrans('', '', string.punctuation)).replace("'", "") for word in x.lower().split()]))
+            text = " ".join(corpus["Text"].astype(str).tolist()).lower()
 
-    # Replace line breaks with spaces
-    if 'Text' in df.columns:
-        df['Text'] = df['Text'].str.replace('\n', '  ')
+            # Generate the word cloud
+            wordcloud = WordCloud(width=800, height=400, background_color="white").generate(text.replace("'", ""))
 
-    # Show dataframe
-    st.write(df)
-    st.markdown(download_csv(df), unsafe_allow_html=True)
+            # Display the word cloud
+            plt.figure(figsize=(10, 5))
+            plt.imshow(wordcloud, interpolation='bilinear')
+            plt.axis("off")
+            st.pyplot()
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+
+    elif option == "Collocation":
+        st.markdown("---")
+        st.subheader("Collocation")
+        # Add a file uploader for the CSV files
+        uploaded_files = st.file_uploader("Upload CSV files", accept_multiple_files=True, type=["csv"])
+
+        # Merge the uploaded CSV files into a single DataFrame
+        if uploaded_files:
+            corpus = pd.DataFrame() # Initialize an empty DataFrame to hold the merged corpus
+            total_words = 0 # Initialize a variable to hold the total number of words
+            for file in uploaded_files:
+                file.seek(0) # Reset the file pointer to the beginning of the file
+                data = pd.read_csv(file)
+                corpus = pd.concat([corpus, data], ignore_index=True)
+                total_words += data["Text"].str.split().str.len().sum() # Compute the number of words for the current file
+            
+           
+            # Display the total number of rows and words
+            st.write(f"Total articles: {len(corpus)}")
+            st.write(f"Total words: {total_words}")
+            
+            # Display an overview of the number of rows per value of the Publication column
+            publication_counts = corpus["Publication"].value_counts().to_frame().reset_index()
+            publication_counts.columns = ["Publication", "Count"] # Rename the columns
+            st.write("Articles by Publication:")
+            st.table(publication_counts)
+            st.markdown("---")
+            st.markdown("Merged dataframe")
+            st.write(corpus)
+
+        # Save collocations as CSV
+        def download_csv(collocations_df):
+            csv = collocations_df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()
+            href = f'<a href="data:file/csv;base64,{b64}" download="collocations.csv">Export to CSV</a>'
+            return href
+
+
+        # Extract the possible collocations
+        if 'corpus' in locals() and st.button("Extract collocations"):
+                # Show a spinner while the collocations are being extracted
+                with st.spinner("Extracting collocations..."):
+                    collocations_df = extract_collocations(corpus, st)
+
+                # Hide the spinner and display the top collocations and their frequencies in a table
+                st.success("Collocations extracted!")
+                st.write("Top collocations:")
+                st.dataframe(collocations_df.head(50))
+                st.markdown(download_csv(collocations_df), unsafe_allow_html=True)
+
+                st.markdown("---")
+
+                st.write("Collocations with POS pattern 'NOUN + ADJ':")
+                noun_adj_df = collocations_df[collocations_df['pos_pattern'] == 'NOUN + ADJ']
+                st.dataframe(noun_adj_df)
+                st.write("Collocations with POS pattern 'NOUN + NOUN':")
+                noun_noun_df = collocations_df[collocations_df['pos_pattern'] == 'NOUN + NOUN']
+                st.dataframe(noun_noun_df)
+                st.write("Collocations with POS pattern 'VERB + NOUN':")
+                verb_noun_df = collocations_df[collocations_df['pos_pattern'] == 'VERB + NOUN']
+                st.dataframe(verb_noun_df)
+
+
+    elif option == "Key Words in Context":
+        st.markdown("---")
+        st.subheader("Key Words in Context")
+        # Add a file uploader for the CSV files
+        uploaded_files = st.file_uploader("Upload CSV files", accept_multiple_files=True, type=["csv"])
+
+        # Merge the uploaded CSV files into a single DataFrame
+        if uploaded_files:
+            corpus = pd.DataFrame() # Initialize an empty DataFrame to hold the merged corpus
+            total_words = 0 # Initialize a variable to hold the total number of words
+            for file in uploaded_files:
+                file.seek(0) # Reset the file pointer to the beginning of the file
+                data = pd.read_csv(file)
+                corpus = pd.concat([corpus, data], ignore_index=True)
+                total_words += data["Text"].str.split().str.len().sum() # Compute the number of words for the current file
+            
+           
+            # Display the total number of rows and words
+            st.write(f"Total articles: {len(corpus)}")
+            st.write(f"Total words: {total_words}")
+            
+            # Display an overview of the number of rows per value of the Publication column
+            publication_counts = corpus["Publication"].value_counts().to_frame().reset_index()
+            publication_counts.columns = ["Publication", "Count"] # Rename the columns
+            st.write("Articles by Publication:")
+            st.table(publication_counts)
+            st.markdown("---")
+            st.markdown("Merged dataframe")
+            st.write(corpus)
+            st.markdown("---")
+            col = st.selectbox("Select a column to search:", options=data.columns)
+
+        # Let the user choose the column to search and the keyword to search for
+        keyword = st.text_input("Enter a keyword to search for:")
+        
+        # Generate and display the concordance
+        if st.button("Generate Concordance"):
+            concordance_df = display_concordance(data, col, keyword)
+            for index, row in concordance_df.iterrows():
+                kwic = row['KWIC'].replace(keyword, keyword)
+                concordance_df.at[index, 'KWIC'] = kwic
+            st.write(concordance_df)
+
 
     # Add a footer with your name
     with st.container():
         st.markdown("---")
-        st.markdown("Developed by [MW Hidayat](https://twitter.com/casecrit)")
+        st.markdown("Developed by MW Hidayat. Find me on [Twitter](https://twitter.com/casecrit)")
 
 # Run the Streamlit app
 if __name__ == '__main__':
-    st.set_page_config(page_title='Article Scraper', page_icon=':newspaper:')
+    st.set_page_config(page_title='SemanText', page_icon=':newspaper:')
     app()
---
+++ text_analysis.py
@@ -0,0 +1,127 @@
+from collections import Counter
+import pandas as pd
+import string
+import streamlit as st
+import re
+import stanza
+import pandas as pd
+from tqdm import tqdm
+import time
+from typing import List
+
+# Load Indonesian stop words
+with open(r'stopwords.txt', 'r') as file:
+        stop_words = file.read().split('\n')
+        stop_words = set(stop_words)
+
+# Define a function to remove stop words
+def stopwords_removal(words):
+    # Remove punctuation characters
+    table = str.maketrans('', '', string.punctuation)
+    words = [w.translate(table) for w in words]
+
+    # Remove stop words
+    return [word for word in words if word.lower() not in stop_words]
+
+# Define a function to plot a bar chart of most frequent words
+def plot_n_most_frequent_words(dataframe, column_name, n=10):
+    # Concatenate all the text data in the specified column
+    text = " ".join(dataframe[column_name].astype(str).tolist()).lower()
+
+    # Use Counter to count the frequency of each word in the text
+    word_counts = Counter(text.split())
+
+    # Get the n most common words and their respective counts
+    top_n_words = word_counts.most_common(n)
+    top_n_words.reverse()  # Reverse the order to display most frequent words on top
+
+    return top_n_words
+
+# Define a function to extract collocations
+def extract_collocations(corpus, progress):
+    # create a Stanza pipeline for Indonesian
+    nlp = stanza.Pipeline('id')
+
+    # Initialize empty lists to store the collocations, pos patterns, and frequencies
+    collocations = []
+    pos_patterns = []
+    frequencies = []
+
+    # Iterate through the rows in the dataframe
+    progress = st.progress(0)
+    # Iterate through the rows in the dataframe
+    start_time = time.time()
+    for index, row in tqdm(corpus.iterrows(), total=corpus.shape[0]):
+        # Extract the collocations using the Stanza library
+        doc = nlp(row['Text'])
+        for sent in doc.sentences:
+            for i, word in enumerate(sent.words):
+                # If the word is a noun and there is a next word, check if it is an adjective or noun
+                if word.pos == 'NOUN' and i < len(sent.words) - 1:
+                    if sent.words[i + 1].pos == 'ADJ':
+                        collocations.append(f"{word.text} {sent.words[i + 1].text}")
+                        pos_patterns.append('NOUN + ADJ')
+                        frequencies.append(1)
+                    elif sent.words[i + 1].pos == 'NOUN':
+                        collocations.append(f"{word.text} {sent.words[i + 1].text}")
+                        pos_patterns.append('NOUN + NOUN')
+                        frequencies.append(1)
+                # If the word is a verb and there is a next word, check if it is a noun
+                elif word.pos == 'VERB' and i < len(sent.words) - 1:
+                    if sent.words[i + 1].pos == 'NOUN':
+                        collocations.append(f"{word.text} {sent.words[i + 1].text}")
+                        pos_patterns.append('VERB + NOUN')
+                        frequencies.append(1)
+
+        # Update the progress bar with elapsed time
+        elapsed_time = time.time() - start_time
+        progress.progress((index + 1) / corpus.shape[0], f"Processed {index+1} articles in {elapsed_time:.2f} seconds")
+
+
+    # Create a new dataframe with the collocations, pos patterns, and frequencies
+    collocations_df = pd.DataFrame({'collocations': collocations, 'pos_pattern': pos_patterns, 'frequency': frequencies})
+    collocations_df = collocations_df.groupby(['collocations', 'pos_pattern']).sum().reset_index()
+    collocations_df.sort_values(by='frequency', ascending=False, inplace=True)
+
+    return collocations_df
+
+# Create a function to generate Concordance - Key Words in Context
+def generate_concordance(text, keyword, context_size=50):
+    lines = text.split('\n')
+    concordance = []
+    for i, line in enumerate(lines):
+        match = re.search(rf"\b{keyword}\b", line, re.IGNORECASE)
+        if match:
+            start = max(0, match.start() - context_size)
+            end = min(len(line), match.end() + context_size)
+            context = line[start:end]
+            context = re.sub(rf"\b{keyword}\b", f"[{keyword}]", context, flags=re.IGNORECASE)
+            concordance.append((i, context))
+    return concordance
+
+def display_concordance(data: pd.DataFrame, col: str, keyword: str, window_size: int = 7) -> pd.DataFrame:
+    concordance_lines = []
+    
+    for index, row in data.iterrows():
+        text = row[col]
+        words = text.split()
+        
+        for i, word in enumerate(words):
+            if word == keyword:
+                start = max(0, i - window_size)
+                end = min(len(words), i + window_size + 1)
+                left_context = ' '.join(words[start:i])
+                keyword_in_context = ' '.join(words[i:i+1])  # keyword in context is just the keyword itself
+                right_context = ' '.join(words[i+1:end])
+                
+                concordance_lines.append({
+                    "Left context": left_context,
+                    "KWIC": keyword_in_context,
+                    "Right context": right_context,
+                    "Publication": row["Publication"],
+                    "Title": row["Title"]
+
+                })
+                
+    concordance_df = pd.DataFrame(concordance_lines)
+    return concordance_df
